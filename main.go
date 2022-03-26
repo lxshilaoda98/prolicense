ecd6b9597b0f438f3c0c980b92dd1a0c709e3b9b
lxshilaoda98 <n1n1n1_owner@163.com>
2022-03-26 22:54:51 +0800

    1

--- main.go
+++ main.go
@@ -1,25 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"github.com/lxshilaoda98/prolicense/pojo"
-)
-
 func main() {
-	var li = pojo.LicenseModel{}
-
-	//li.CheckKey("asdiuqwjasksdiasdlwoerfasdwerfas")
-	//传入li的时间。如果为0就是永不过期1
-
-	li.EndTime = 0
-	li.GetKey("Lsdiuqwjasksdiasdlwoerfasdwerfas") //通过公有key获取private的key
-	fmt.Println(li.LicenseKey)
-
-	//通用key
-	//验证key是否正确
-	f := li.CheckKey(li.LicenseKey,
-		"Lsdiuqwjasksdiasdlwoerfasdwerfas")
-	fmt.Println("endtime:", li.EndTime)
-	fmt.Println("f:", f)
+	//var li = pojo.LicenseModel{}
+	//
+	////li.CheckKey("asdiuqwjasksdiasdlwoerfasdwerfas")
+	////传入li的时间。如果为0就是永不过期1
+	//
+	//li.EndTime = 0
+	//li.GetKey("Lsdiuqwjasksdiasdlwoerfasdwerfas") //通过公有key获取private的key
+	//fmt.Println(li.LicenseKey)
+	//
+	////通用key
+	////验证key是否正确
+	//f := li.CheckKey(li.LicenseKey,
+	//	"Lsdiuqwjasksdiasdlwoerfasdwerfas")
+	//fmt.Println("endtime:", li.EndTime)
+	//fmt.Println("f:", f)
 
 }
